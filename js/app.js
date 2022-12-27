602503e5f260d86aa196d07600f815067e84aa7d
aghataoliveira <aghatagoncalves@outlook.com>
2022-12-27 12:46:16 -0500

    udemy course - programming logic using javascript

---
+++ js/app.js
@@ -0,0 +1,58 @@
+//Aula 2.1 - Variáveis
+// var nome = prompt('Qual é o seu nome?');
+
+// alert ('Bem-vindo, ' + nome + '!');
+
+//_______________________________________02.01 - EXERCICIOS__________________________________________
+
+
+// const kiku = prompt('O que vc acha de estudar TI?');
+
+// alert('Eu acho um ' + kiku);
+
+
+
+//____________________________________________________________________________________________
+
+//Aula 02.02 - Conversão
+
+//pedir para o usuário digitar um numero
+// let numberOne = prompt('give me a number');
+// // Aqui eu transformo minha variável para número, pois o browser sempre vai entender o que o que o usuário digita como texto.
+// numberOne = parseFloat(numberOne);
+
+// //pedir para o usuário digitar outro numero
+// let numberTwo = prompt('Give me another number');
+// numberTwo = parseFloat(numberTwo);
+
+// // mostrar a soma
+// alert(numberOne + numberTwo);
+
+//________________________________________02.02 - EXERCICIOS__________________________________________
+
+
+// let n1 = prompt('digite primeiro numero');
+// n1 = parseInt(n1);
+
+// let n2 = prompt ('digite o segundo numero');
+// n2 = parseInt(n2);
+
+// alert("a subtração é: " + (n1-n2));
+// alert("a multiplicação é: " + (n1*n2));
+// alert("a divisão é: " + (n1/n2));
+// alert("a soma é: " + (n1+n2));
+
+
+//________________________________________02.03 - EXERCICIOS__________________________________________
+
+// let n1 = prompt('digite um número ');
+// n1 = parseFloat(n1);
+
+// let n2 = prompt('digite outro número ');
+// n2 = parseFloat(n2);
+
+// let media = (n1 + n2) / 2;
+
+// alert(media);
+
+//________________________________________02.04 - EXERCICIOS__________________________________________
