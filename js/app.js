d1170e9cbf242aac3fe5b5462439b241864c148c
aghataoliveira <aghatagoncalves@outlook.com>
2023-01-03 21:00:46 -0500

    function to count to 1001

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const output = document.querySelector('#output');
 var mensagem = '';
 
 for (var i = 0; i < 1001; i++) {
-    mensagem += 'i: ' + i + '<br>';
+    mensagem += 'indice: ' + i + '<br>';
 }
 
 output.innerHTML = mensagem;
